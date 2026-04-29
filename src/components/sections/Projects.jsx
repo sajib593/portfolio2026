9d514c0a684a25dd76361231744051698ae079ae
sajib593 <sajib.hoossain11@gmail.com>
2026-04-28 22:40:13 -0700

    projectsJsx

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,19 +1,23 @@
-import { projects } from "../../data/ProjectsData";
+import Container from "../layout/Container";
 import Card from "../ui/Card";
-
+import { projects } from "../../data/ProjectsData";
 
 const Projects = () => {
-    return (
-        <section>
-      <h2>My Projects</h2>
+  return (
+    <section className="section">
+      <Container>
 
-      <div>
-        {projects.map((p) => (
-          <Card key={p.id} {...p} />
-        ))}
-      </div>
+        <h2 className="heading-lg mb-10">My Projects</h2>
+
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {projects.map((p) => (
+            <Card key={p.id} {...p} />
+          ))}
+        </div>
+
+      </Container>
     </section>
-    );
+  );
 };
 
 export default Projects;
